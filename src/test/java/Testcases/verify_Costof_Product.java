f215e897248a3853a4164c37351a040486a124da
Prabhakaran G <prabhakaranguru123@gmail.com>
2024-07-13 17:45:53 +0530

    Project Stucture Added

---
+++ src/test/java/Testcases/verify_Costof_Product.java
@@ -0,0 +1,41 @@
+package Testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+
+public class verify_Costof_Product {
+    public WebDriver driver=null;
+    @BeforeTest
+    public void setup(){
+        driver=new FirefoxDriver();
+        driver.get("https://clicks.aweber.com/y/ct/?l=GoDhLn&m=mcNYuYFeLUQLjy9&b=h5T4l2fu1DOGeQSqXkdkzQ");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+    @Test
+    public void Testcase(){
+        driver.findElement(By.linkText("MOBILE")).click();
+        String rate1=driver.findElement(By.xpath("//span[contains(text(),'$100.00')]")).getText();
+        driver.findElement(By.cssSelector("a[title='Xperia']")).click();
+        String rate2=driver.findElement(By.xpath("//*[@id=\"product-price-1\"]/span")).getText();
+        SoftAssert softAssert=new SoftAssert();
+        try {
+            softAssert.assertEquals(rate1,rate2);
+        }
+        catch (AssertionError e ){
+            e.printStackTrace();
+        }
+        softAssert.assertAll();
+
+    }
+    @AfterTest
+    public void teardown(){
+        driver.quit();
+    }
+}
--- src/test/java/Testcases/verify_item_mobilelistpage_sorted_by_name.java
+++ src/test/java/Testcases/verify_item_mobilelistpage_sorted_by_name.java
@@ -5,22 +5,43 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class verify_item_mobilelistpage_sorted_by_name {
-    static WebDriver driver=null;
-    public static void main(String[] args) {
+    public WebDriver driver=null;
+    @BeforeTest
+    public void setup(){
+        driver=new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+
+    @Test
+    public  void Testcase() {
         try{
-            driver=new FirefoxDriver();
             driver.get("https://clicks.aweber.com/y/ct/?l=GoDhLn&m=mcNYuYFeLUQLjy9&b=h5T4l2fu1DOGeQSqXkdkzQ");
             String homepagetittle=driver.getTitle();
             SoftAssert softAssert=new SoftAssert();
-            softAssert.assertEquals(homepagetittle,"THIS IS DEMO SITE");
+            Boolean tittle1=homepagetittle.equals("THIS IS DEMO SITE");
+            if (tittle1.equals(Boolean.TRUE)){
+                System.out.println("The HOME PAGE tittle is present");
+            }else {
+                System.out.println("The tittle is Not Present");
+            }
             driver.findElement(By.linkText("MOBILE")).click();
             String mobilepagatittle=driver.getTitle();
+            try{
+                softAssert.assertEquals(mobilepagatittle,"Mobile");
+            }catch (AssertionError e){
+                e.printStackTrace();
+            }
             softAssert.assertEquals(mobilepagatittle,"Mobile");
             Select select=new Select(driver.findElement(By.xpath("//*[@id=\"top\"]/body/div[1]/div/div[2]/div/div[2]/div[1]/div[3]/div[1]/div[1]/div/select")));
             select.selectByVisibleText("Name");
@@ -32,18 +53,20 @@ public class verify_item_mobilelistpage_sorted_by_name {
             }
             List newnamelist= new ArrayList<>(namelist);
             Collections.sort(newnamelist);
-            softAssert.assertEquals(namelist,newnamelist);
+            try{
+                softAssert.assertEquals(namelist,newnamelist);
+            }catch (AssertionError e){
+                e.printStackTrace();
+            }
             softAssert.assertAll();
             System.out.println("All Products are Sorted ");
         }
         catch (AssertionError e) {
             e.printStackTrace();
         }
-        finally {
-            driver.quit();
-        }
-
-
-
+    }
+    @AfterTest
+    public void teardown(){
+        driver.quit();
     }
 }
